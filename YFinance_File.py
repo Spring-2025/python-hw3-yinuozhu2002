88aed184b44ec3f1607a23168062da67fe6462a6
yinuozhu2002 <yz4836@columbia.edu>
2025-03-05 00:36:20 -0500

    Update YFinance_File.py

--- YFinance_File.py
+++ YFinance_File.py
@@ -2,28 +2,15 @@ import yfinance as yf
 import pandas as pd
 import numpy as np
 
-def get_stock_data(symbol):
+def get_stock_data(symbol, start=None, end=None):
   
-    data = yf.download(symbol)
-    expected_cols = ['Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume']
-    for col in expected_cols:
-        if col not in data.columns:
-            data[col] = np.nan
-
+    data = yf.download(symbol, start=start, end=end)
     return data
 
 def YahooData2returns(YahooData):
-  
-    # 1) Extract all columns
-    open_prices = YahooData['Open']
-    high_prices = YahooData['High']
-    low_prices  = YahooData['Low']
-    close_prices = YahooData['Close']
-    adj_close_prices = YahooData['Adj Close']
-    volume_data = YahooData['Volume']
-
-    # 2) Use 'Adj Close' to compute returns
-    pricevec = adj_close_prices.values
+   
+    prices = YahooData['Adj Close']
+    pricevec = prices.values
     returns = (pricevec[1:] / pricevec[:-1]) - 1
-
     return returns
+
