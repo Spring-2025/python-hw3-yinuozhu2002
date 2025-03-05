3fe65821f01bc505af78d59b4f7f1546d081e9f3
yinuozhu2002 <yz4836@columbia.edu>
2025-03-04 23:16:56 -0500

    Update YFinance_File.py

--- YFinance_File.py
+++ YFinance_File.py
@@ -5,9 +5,4 @@ import numpy as np
 def YahooData2returns(YahooData):
     prices = YahooData['Adj Close']
     returns = prices.pct_change().dropna()
-    return returns.values
-
-def get_stock_data(symbol):
-    data = yf.download(symbol)
-    prices = data['Adj Close']
-    return prices
+    return returns
