8de4bb5fcd860213061fa4f163131ef6edd591ed
yinuozhu2002 <yz4836@columbia.edu>
2025-03-04 23:44:31 -0500

    Update VaR_File.py

--- VaR_File.py
+++ VaR_File.py
@@ -2,11 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import norm
 
-
-import numpy as np
-import matplotlib.pyplot as plt
-from scipy.stats import norm
-
 def VaR(r, confidence, principal=1):
     alpha = 1 - confidence
     var_value = np.percentile(r, alpha * 100)
