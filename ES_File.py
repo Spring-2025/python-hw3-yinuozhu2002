57e38825aac6d344d54278b89d39259e62b74b41
yinuozhu2002 <yz4836@columbia.edu>
2025-03-04 21:43:41 -0500

    Update ES_File.py

--- ES_File.py
+++ ES_File.py
@@ -2,9 +2,11 @@ import numpy as np
 
 
 def ES(losses, confidence=None, VaR=None, use_PnL=False):
-
+    if confidence is not None:
+        alpha = 1 - confidence
     if VaR is None:
         VaR = np.percentile(losses, 100 * (1 - confidence))
+        
     es_value = np.mean(losses[losses > VaR])
 
     return es_value
